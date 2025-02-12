956c87140ebeae8c2c5945561909abcf9f0a8392
leanarchie <leane.ponchaut@isen-ouest.yncrea.fr>
2025-02-12 15:47:22 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import EventDetails from "./pages/EventDetails";
+import EventPage from "./pages/EventPage";
 import CartPage from "./pages/CartPage";
-import Navbar from "./context/Navbar";
+import Navbar from "./components/Navbar";
 import { CartProvider } from "./context/CartContext";
 import "./styles/global.css";
 
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/event/:id" element={<EventDetails />} />
+          <Route path="/event/:id" element={<EventPage />} />
           <Route path="/cart" element={<CartPage />} />
         </Routes>
       </Router>
